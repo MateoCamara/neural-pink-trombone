0968a320ac8e099d2b34fd3ed469738571e4aa37
Mateo Cámara <mateo.camara.largo@alumnos.upm.es>
2024-06-03 09:33:12 +0200

    Added schedulers to train

---
+++ run_scheduler_1.sh
@@ -0,0 +1,44 @@
+#python run.py -c configs/config_betaVAESynth_dynamic_0.yaml
+#python run.py -c configs/config_betaVAESynth_dynamic_1.yaml
+#python run.py -c configs/config_betaVAESynth_dynamic_2.yaml
+
+#python run.py -c configs/config_wav2vec_dynamic_0.yaml
+#python run.py -c configs/config_wav2vec_dynamic_1.yaml
+#python run.py -c configs/config_wav2vec_dynamic_2.yaml
+
+#python run.py -c configs/config_encodec_dynamic_0.yaml
+#python run.py -c configs/config_encodec_dynamic_1.yaml
+#python run.py -c configs/config_encodec_dynamic_2.yaml
+
+#python run.py -c configs/config_betaVAESynth_dynamic_10changes_0.yaml
+#python run.py -c configs/config_betaVAESynth_dynamic_10changes_1.yaml
+#python run.py -c configs/config_betaVAESynth_dynamic_10changes_2.yaml
+#
+#python run.py -c configs/config_wav2vec_dynamic_10changes_0.yaml
+#python run.py -c configs/config_wav2vec_dynamic_10changes_1.yaml
+#python run.py -c configs/config_wav2vec_dynamic_10changes_2.yaml
+
+python run.py -c configs/config_encodec_dynamic_10changes_0.yaml
+python run.py -c configs/config_encodec_dynamic_10changes_1.yaml
+python run.py -c configs/config_encodec_dynamic_10changes_2.yaml
+
+#python run.py -c configs/config_betaVAESynth_0.yaml
+#python run.py -c configs/config_betaVAESynth_1.yaml
+#python run.py -c configs/config_betaVAESynth_2.yaml
+#
+#python run.py -c configs/config_wav2vec_0.yaml
+#python run.py -c configs/config_encodec_0.yaml
+#python run.py -c configs/config_betaVAE_0.yaml
+#
+#python run.py -c configs/config_betaVAESynth_dynamic_lengua_0.yaml
+#python run.py -c configs/config_betaVAESynth_dynamic_lengua_1.yaml
+#python run.py -c configs/config_betaVAESynth_dynamic_lengua_2.yaml
+#
+#python run.py -c configs/config_wav2vec_dynamic_lengua_0.yaml
+#python run.py -c configs/config_wav2vec_dynamic_lengua_1.yaml
+#python run.py -c configs/config_wav2vec_dynamic_lengua_2.yaml
+#
+#python run.py -c configs/config_encodec_dynamic_lengua_0.yaml
+#python run.py -c configs/config_encodec_dynamic_lengua_1.yaml
+#python run.py -c configs/config_encodec_dynamic_lengua_2.yaml
+
---
+++ run_scheduler_2.sh
@@ -0,0 +1,18 @@
+#python run.py -c configs/config_betaVAESynth_dynamic_0.yaml
+python run.py -c configs/config_betaVAESynth_dynamic_2.yaml
+
+#python run.py -c configs/config_wav2vec_dynamic_0.yaml
+python run.py -c configs/config_wav2vec_dynamic_2.yaml
+
+python run.py -c configs/config_encodec_dynamic_2.yaml
+
+python run.py -c configs/config_betaVAESynth_dynamic_10changes_1.yaml
+python run.py -c configs/config_betaVAESynth_dynamic_10changes_2.yaml
+
+python run.py -c configs/config_wav2vec_dynamic_10changes_2.yaml
+
+python run.py -c configs/config_encodec_dynamic_10changes_1.yaml
+python run.py -c configs/config_encodec_dynamic_10changes_2.yaml
+
+python run.py -c configs/config_wav2vec_0.yaml
+python run.py -c configs/config_encodec_0.yaml
