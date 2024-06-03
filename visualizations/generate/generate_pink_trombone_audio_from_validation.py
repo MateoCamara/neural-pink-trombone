1bb6df8edeeb09791239fdb5305cc289aae050d2
Mateo Cámara <mateo.camara.largo@alumnos.upm.es>
2024-06-03 09:32:03 +0200

    Changed device

--- visualizations/generate/generate_pink_trombone_audio_from_validation.py
+++ visualizations/generate/generate_pink_trombone_audio_from_validation.py
@@ -1,5 +1,5 @@
 import os
-os.environ["CUDA_VISIBLE_DEVICES"] = ""
+# os.environ["CUDA_VISIBLE_DEVICES"] = ""
 from scipy.io.wavfile import write
 import numpy as np
 import torch
@@ -12,14 +12,16 @@ from visualizations.visualization_utils import load_model, load_dataloader, deno
 
 NUMERO_DE_EJEMPLOS = 50
 
-all_available_configs = os.listdir('../configs')
+all_available_configs = os.listdir('../../configs')
 
 for config_path in all_available_configs:
     print(config_path)
     if config_path == 'config_betaVAE_0.yaml':
         continue
 
-    with open(f'../configs/{config_path}', 'r') as file:
+    device = 'cpu'
+
+    with open(f'../../configs/{config_path}', 'r') as file:
         try:
             config = yaml.safe_load(file)
         except yaml.YAMLError as exc:
@@ -30,17 +32,17 @@ for config_path in all_available_configs:
         continue
     version_number = "version_" + config_path.split('_')[-1].split('.')[0]
 
-    state_dict_path = os.path.join("../logs", model_name, version_number, "checkpoints", "last.ckpt")
+    state_dict_path = os.path.join("../../logs", model_name, version_number, "checkpoints", "last.ckpt")
     if not os.path.exists(state_dict_path):
         continue
 
-    model = load_model(config['model_params']['name'], config).to('cpu')
-    state_dict = torch.load(state_dict_path, map_location=torch.device('cpu'))['state_dict']
+    model = load_model(config['model_params']['name'], config).to(device)
+    state_dict = torch.load(state_dict_path, map_location=torch.device(device))['state_dict']
     fixed_state_dict = {k.replace("model.", ""): v for k, v in state_dict.items()}
     model.load_state_dict(fixed_state_dict)
 
     data_type = config['data_params']['data_type']
-    data_path = os.path.join('..', config['data_params']['data_path'])
+    data_path = os.path.join('../..', config['data_params']['data_path'])
     data_path = os.path.expanduser(data_path)
     dataset, json_file = load_dataloader(data_type)
     val_dataset = dataset(os.path.join(data_path, "test"), os.path.join(data_path, json_file[1]), **config['data_params'])
@@ -48,7 +50,7 @@ for config_path in all_available_configs:
 
     for i, batch in tqdm(enumerate(val_loader)):
         regen_batch = model.forward(input=batch[0], params=batch[1])
-        save_dir = os.path.join("../../generated_samples/", model_name + "_" + version_number)
+        save_dir = os.path.join("../../../generated_samples/", model_name + "_" + version_number)
         os.makedirs(save_dir, exist_ok=True)
         save_path = os.path.join(save_dir, f"sample_{i}")
         os.makedirs(save_path, exist_ok=True)
